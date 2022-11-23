30c4b3eee04937a03b3082ad9ac2f0671e168bbc
DALPHINMARY <dalphintpm@gmail.com>
2022-11-23 13:10:56 +0530

    addeded

---
+++ travelproject/credentials/views.py
@@ -0,0 +1,29 @@
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def register(request):
+    if request.method =='POST':
+        username=request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['password1']
+        if password==cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username Taken")
+                return redirect('register')
+            if User.objects.filter(email=email).exists():
+                messages.info(request,"email Taken")
+                return redirect('register')
+            else:
+                user= User.objects.create_user(username=username,password=password,first_name=first_name,last_name=last_name,email=email)
+                user.save();
+        else:
+            messages.info(request, "password not matched")
+            return redirect('register')
+        return redirect('/')
+        return render(request,"register.html")
